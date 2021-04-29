6373a6b5f3717811f9a85c0479cb4efd6e759991
abhignavuppala01@gmail.com <abhiv053@gmail.com>
2021-04-29 09:51:43 +0530

    second commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.get("/",function(req,res){
 });
 app.get("/home",function(req,res){
     res.send("home called");
-});
+});//
 app.get("/login",function(req,res){
     res.send("login called");
 });
