e694f9c55442e0d21325bb8487c1185df5a25e9c
abhignavuppala01@gmail.com <abhiv053@gmail.com>
2021-04-30 09:39:03 +0530

    added resume.html

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.get("/home",function(req,res){
 app.get("/login",function(req,res){
     res.send("login called");
 });
+app.get("/resume",function(req,res){
+    let indexFilepath=__dirname+"/resume.html";
+    res.sendFile(indexFilepath); 
+});
 const port=process.env.PORT || 3000;
 app.listen(port,function()
 {
