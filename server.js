82cf02007e0dc6df96613534f30abce6802ef060
abhignavuppala01@gmail.com <abhiv053@gmail.com>
2021-05-20 22:59:55 +0530

    third commit

--- server.js
+++ server.js
@@ -1,24 +1,12 @@
 
 const express=require('express');
 const path = require('path');
-const mongoose =require ('mongoose');
 const app=express();
-const courselib = require('./backend/lib/courselib');
-const connect = require('./backend/lib/dbconnect');
-const config = require('./backend/config/config');
 
-// var password=process.env.MONGO_atlas_password;
-// var connectionString="mongodb+srv://abhi:"+password+"@cluster0.hhb8k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-// mongoose.connect(connectionString,{useNewUrlParser: true,useUnifiedTopology: true});
-// mongoose.connection.on('connected',function(){
-//     console.log('Database connected');
-// });
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(__dirname+"/frontend"));
-app.use(express.static(path.join(__dirname, "static")));
-connect.connect();
 
 
 app.get("/",function(req,res)
@@ -27,15 +15,6 @@ app.get("/",function(req,res)
 });
 
 
-app.get("/crudd",function(req,res)
-{
-    filePathName=__dirname+'/static/html/crud.html';
-    res.sendFile(filePathName);
-});
-app.get("/crud", courselib.getall);
-app.delete("/crud/:idd", courselib.deleteone);
-app.put("/crud/:idd", courselib.update);
-app.post("/crud",courselib.addnewone);
 
 
 app.get("/google",function(req,res)
@@ -81,8 +60,7 @@ app.get("/register",function(req,res)
     res.sendFile(fp);
 });
 
-const PORT = config.webPort;
-app.listen(PORT||3000,function()
+app.listen(process.env.PORT||3000,function()
 {
     console.log("server started on port 3000 ");
 });
